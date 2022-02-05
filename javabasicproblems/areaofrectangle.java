8bbfa8663308310eb815a59ba6542d6e7e7e4cd2
krishnamohan1122 <krishnamohan72452964@gmail.com>
2022-02-05 18:13:33 +0530

    Area of rectangle

---
+++ javabasicproblems/areaofrectangle.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class areaofrectangle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the width of the rectangle : ");
+        int width = input.nextInt();
+        System.out.print("Enter the height of the rectangle : ");
+        int height = input.nextInt();
+        double area = width * height;
+        System.out.println("Area of a rectangle is: " + area);
+    }
+}
