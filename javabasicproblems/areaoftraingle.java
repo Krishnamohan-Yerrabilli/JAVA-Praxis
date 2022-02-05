5ec06035f43961bf5215038b46122209191e45e1
krishnamohan1122 <krishnamohan72452964@gmail.com>
2022-02-05 18:00:52 +0530

    Area_of_a_traingle

---
+++ javabasicproblems/areaoftraingle.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class areaoftraingle {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius of the triangle : ");
+        int base = input.nextInt();
+        System.out.print("Enter the height of the triangle : ");
+        int height = input.nextInt();
+        double area = 0.5 * base * height;
+        System.out.println("Area of a circle is: " + area);
+    }
+}
