5da28de953b00288d9dd60e3e983f360c3847df6
krishnamohan1122 <krishnamohan72452964@gmail.com>
2022-02-07 22:34:05 +0530

    Addition of two numbers

---
+++ javabasicprograms-sec2/Sum_of_two_numbers.java
@@ -0,0 +1,17 @@
+//Take integer inputs till the user enters 0 and print the largest number from all.
+
+package com.company;
+
+import java.util.Scanner;
+
+public class Sum_of_two_numbers {
+    public static void main(String[] args) {
+        System.out.print("\nEnter the first number : ");
+        Scanner in = new Scanner(System.in);
+        int input_1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        int input_2 = in.nextInt();
+        int result = input_1 + input_2;
+        System.out.println("\nThe Addition of two numbers : " + result);
+    }
+}
