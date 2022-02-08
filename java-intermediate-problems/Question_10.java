3aa54d7e619733f672ddd2a699995321283068a5
krishnamohan1122 <krishnamohan72452964@gmail.com>
2022-02-08 07:25:49 +0530

    Calculate Commission percentage

---
+++ java-intermediate-problems/Question_10.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+public class Question_10 {
+    public static void main(String[] args){
+        // Calculate Commission Percentage
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the amount of sales: ");
+        double sales = in.nextInt();
+        System.out.print("Enter the commission percentage: ");
+        double comper = in.nextInt();
+        double commission = (comper/100) * sales;
+        System.out.println("You're commission amount is " + commission);
+    }
+}
