9201fffcfb1a3cb742b8b18e9151fdc71cbed53c
krishnamohan1122 <krishnamohan72452964@gmail.com>
2022-02-13 08:41:31 +0530

    Check he/she is eligible to vote if person 18 and above they are eligible to vote

---
+++ java-fuctions-programs/Vote.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Vote {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        fun(in.nextInt());
+
+    }
+    static void fun(int a){
+        if (a >= 18){
+            System.out.println("Your Eligible to vote");
+        } else {
+            System.out.println("Your Not Eligible to vote");
+        }
+    }
+}
