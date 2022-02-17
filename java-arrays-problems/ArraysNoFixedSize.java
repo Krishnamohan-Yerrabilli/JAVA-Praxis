5bea7f63bbe18dbe9b1eab8aef6cd6ed51ce61a6
krishnamohan1122 <krishnamohan72452964@gmail.com>
2022-02-17 07:58:49 +0530

    Multi_Dimensional Arrays with no fixed size

---
+++ java-arrays-problems/ArraysNoFixedSize.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class ArraysNoFixedSize {
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1},
+                {4, 5, 6},
+                {8, 9, 10, 11},
+                {10, 2}
+        };
+
+        for(int row = 0; row < arr.length ; row++){
+            for(int col = 0; col < arr[row].length ; col++){
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
