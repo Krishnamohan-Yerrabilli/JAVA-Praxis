ea015b0bcc508893ec21fc16d665158e47348155
krishnamohan1122 <krishnamohan72452964@gmail.com>
2022-02-17 07:39:54 +0530

    Single dimensional arrays (only 1 row)

---
+++ java-arrays-problems/Arrays.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Arrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int[] arr = new int[3];
+        for (int row = 0; row < arr.length; row++) {
+            arr[row] = in.nextInt();
+        }
+
+//        for (int row = 0;row < arr.length; row++)
+        for (int a : arr){
+            System.out.print(a + " ");
+        }
+    }
+}
