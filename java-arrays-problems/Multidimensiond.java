ee7aa56926436082da3b2d9d6f4bc7019a2035c3
krishnamohan1122 <krishnamohan72452964@gmail.com>
2022-02-23 06:30:30 +0530

    Executing Multi-dimensional declaration and initialization

---
+++ java-arrays-problems/Multidimensiond.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Multidimensiond {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int[][] arr = new int[3][3];
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                arr[row][col] = in.nextInt();
+            }
+        }
+
+        for (int row = 0; row < arr.length; row++) {
+            for (int col = 0; col < arr[row].length; col++) {
+                System.out.print(arr[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
