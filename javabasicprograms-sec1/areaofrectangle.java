ee5697e01451acf498e7d73f33b72f915f0d2219
Krishnamohan Yerrabilli <58173938+Krishnamohan-Yerrabilli@users.noreply.github.com>
2022-03-15 23:23:41 +0530

    Update areaofrectangle.java

--- javabasicprograms-sec1/areaofrectangle.java
+++ javabasicprograms-sec1/areaofrectangle.java
@@ -7,7 +7,7 @@ public class areaofrectangle {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the width of the rectangle : ");
         int width = input.nextInt();
-        System.out.print("Enter the height of the rectangle : ");
+        System.out.print("Enter the height  of the rectangle : ");
         int height = input.nextInt();
         double area = width * height;
         System.out.println("Area of a rectangle is: " + area);
