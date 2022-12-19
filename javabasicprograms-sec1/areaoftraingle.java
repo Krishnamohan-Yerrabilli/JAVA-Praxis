e20dee9dfae3c2a1972cb46b99e47d48f0eaea70
Krishnamohan Yerrabilli <krishnamohan72452964@gmail.com>
2022-12-19 18:35:11 +0530

    Updated desc

--- javabasicprograms-sec1/areaoftraingle.java
+++ javabasicprograms-sec1/areaoftraingle.java
@@ -10,6 +10,6 @@ public class areaoftraingle {
         System.out.print("Enter the height of the triangle : ");
         int height = input.nextInt();
         double area = 0.5 * base * height;
-        System.out.println("Area of a circle is: " + area);
+        System.out.println("Area of a circle is going to be: " + area);
     }
 }
