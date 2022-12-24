0ed597aff6ab917387977865e3b38baf40fd080b
Krishnamohan Yerrabilli <krishnamohan72452964@gmail.com>
2022-12-24 21:41:17 +0530

    Update Prism_volume.java

--- javabasicprograms-sec2/Prism_volume.java
+++ javabasicprograms-sec2/Prism_volume.java
@@ -7,9 +7,9 @@ public class Prism_volume{
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the width of prism : ");
         double width = input.nextDouble();
-        System.out.print("Enter the length of the prism : ");
+        System.out.print("Enter the length of the prism : "); /* Reading the numeric value from the user */
         double length = input.nextDouble();
-        System.out.print("Enter the height of the prism : ");
+        System.out.print("Enter the height of the prism : "); /* Reading the numeric value of height from the user */
         double height = input.nextDouble();
         double parameter = width * length * height;
         System.out.println("Volume of the prism is: " + (float)(parameter));
